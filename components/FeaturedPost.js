fdc045372575a8c7bb3d560befc00085e06c39dd
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-27 22:47:53 +0700

    use context

---
+++ components/FeaturedPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedPost = () => {
+  return (
+    <article>
+      
+    </article>
+  )
+}
+
+export default FeaturedPost
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import FeaturedPost from '../components/FeaturedPost'
 import Navbar from '../components/Navbar/Navbar'
 
 
@@ -6,7 +7,8 @@ export default function Home() {
 
   return (
    <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white font-patrick">
-    <Navbar />     
+    <Navbar />
+    <FeaturedPost />     
    </div>
   )
 }
