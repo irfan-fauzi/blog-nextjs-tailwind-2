e150afc8a7e832de455b7b0c6bf99ff86b220fff
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-27 23:52:40 +0700

    container

--- components/FeaturedPost.js
+++ components/FeaturedPost.js
@@ -14,7 +14,20 @@ const FeaturedPost = () => {
             </div>
             <span>&bull;</span>
             <div className="">
-              J12 july 2021
+              12 july 2021
+            </div>
+          </div>
+          <h2 className="text-2xl mt-6">Understanding color theory: the color wheel and finding complementary colors</h2>
+          <p className="text-white/60 mt-6 w-8/12 ">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+          </p>
+          <div className="flex item-center mt-6">
+            <img src="/author-1.jpg" alt="" className="w-14 h-14 rounded-full object-cover" />
+            <div className="ml-4">
+              <h3>Irfan fauzi</h3>
+              <div className="text-white/50 text-sm">
+                UI Designer
+              </div>
             </div>
           </div>
         </div>
