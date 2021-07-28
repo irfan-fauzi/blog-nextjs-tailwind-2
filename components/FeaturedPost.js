6333e25ef12c40bf4d640215159c62eab0bb8d3e
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-28 10:46:11 +0700

    costumize container

--- components/FeaturedPost.js
+++ components/FeaturedPost.js
@@ -3,11 +3,11 @@ const FeaturedPost = () => {
   
   return (
     <article>
-      <div className="flex -mx-6">
+      <div className="flex -mx-6 items-center">
         <div className="px-6 w-8/12">
           <img src="/thumbnail-1.jpg" alt="" className="rounded-xl w-full" />
         </div>
-        <div className="w-4/12 px-6">
+        <div className="w-4/12 px-4">
           <div className="flex items-center text-white/60 space-x-2">
             <div className="uppercase">
               UI design
@@ -17,8 +17,8 @@ const FeaturedPost = () => {
               12 july 2021
             </div>
           </div>
-          <h2 className="text-2xl mt-6">Understanding color theory: the color wheel and finding complementary colors</h2>
-          <p className="text-white/60 mt-6 w-8/12 ">
+          <h2 className="text-3xl mt-4">Understanding color theory: the color wheel and finding complementary colors</h2>
+          <p className="text-white/60 mt-4 w-10/12 ">
           Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
           </p>
           <div className="flex item-center mt-6">
